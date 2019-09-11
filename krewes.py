b1491e351579a6c2b01ee7c5c20cac3488701cab
Kazi Jamal <jamalkazi02@gmail.com>
2019-09-10 22:12:13 -0400

    added second method to solve krewes problem

--- krewes.py
+++ krewes.py
@@ -1,8 +1,20 @@
 import random
 
-KREWES = {'orpheus':['john','jeff'],'rex':['ashley','jake'],'endymion':['david','dan']}
+KREWES = {
+	'orpheus': ['Emily', 'Kevin', 'Vishwaa', 'Eric', 'ray', 'Jesse', 'Tiffany', 'Amanda', 'Junhee', 'Jackie ', 'Tyler', 'Emory', 'Ivan', 'Elizabeth', 'Pratham', 'Shaw', 'Eric', 'Yaru', 'Kelvin', 'Hong Wei', 'Michael', 'Kiran', 'Amanda', 'Joseph', 'Tanzim', 'David', 'Yevgeniy'],
+	'rex': ['William', 'Joseph', 'Calvin', 'Ethan', 'Moody', 'Mo', 'Big Mo', 'Peihua', 'Saad', 'Benjamin', 'Justin', 'Alice', 'Hilary', 'Ayham', 'Michael', 'Matthew', 'Jionghao', 'Devin ', 'David', 'Jacob', 'Will', 'Hannah', 'Alex'],
+	'endymion': ['Grace', 'Nahi', 'Derek', 'Jun Tao', 'Connor', 'Jason', 'Tammy', 'Albert', 'Kazi', 'Derek', 'Brandon', 'Kenneth', 'Lauren', 'Biraj', 'Jeff', 'Jackson', 'Taejoon', 'Kevin', 'Jude', 'Sophie', 'Henry', 'Coby', 'Manfred', 'Leia', 'Ahmed', 'Winston']
+}
 
-def randStudent(dict):
-    return random.choice(dict[random.choice(dict.keys())])
+def randStudent(d):
+  return random.choice(d[random.choice(d.keys())])
+
+def randStudentv2(d):
+  masterList = []
+  for l in d.values():
+    masterList += l
+  randNum = random.randint(0,len(masterList)-1)
+  return masterList[randNum]
 
 print(randStudent(KREWES))
+print(randStudentv2(KREWES))
