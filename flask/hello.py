cf8df4258140d6d2541d17a3a949cfe94e9add40
Kazi Jamal <jamalkazi02@gmail.com>
2019-09-17 18:40:49 -0400

    added first flask app

---
+++ flask/hello.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+app = Flask(__name__)  # create instance of class Flask
+
+
+@app.route("/")  # assign following fxn to run when root route requested
+def hello_world():
+    print(__name__)  # where will this go?
+    return "No hablo queso!"
+
+
+@app.route("/test_static_page")
+def static_page():
+    print("test static page")
+    return app.send_static_file("static_page.html")
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
