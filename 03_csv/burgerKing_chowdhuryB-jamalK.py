3500b4bb427ed07136c8f422629cd946f0e308e4
Kazi Jamal <jamalkazi02@gmail.com>
2019-09-16 22:51:14 -0400

    updated occupations solution

--- 03_csv/burgerKing_chowdhuryB-jamalK.py
+++ 03_csv/burgerKing_chowdhuryB-jamalK.py
@@ -3,36 +3,47 @@
 # K #06: StI/O: Divine your Destiny!
 # 2019-09-17
 
-import csv
 import random
 
+csv = open("occupations.csv", "r")
+data = csv.read().strip('\n').split('\n')
+
 occupations_dict = {}
-occupations_list = []
 
 
-def init_dict(dict, filename):
-    with open(filename, newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', quotechar='\"')
-        line_count = 0
-        for line in reader:
-            if (line_count == 0):
-                line_count += 1
-                continue
-            dict[line[0]] = float(line[1])
-            line_count += 1
+def init_dict():
+    for entry in data[1:len(data)-1]:
+        if entry.count(',') > 1:
+            occupation = entry.strip('"').split('"')
+            occupations_dict[occupation[0]] = float(occupation[1].strip(','))
+        else:
+            occupation = entry.split(',')
+            occupations_dict[occupation[0]] = float(occupation[1])
 
 
-def rand_occupation(dict, list):
-    occupation_count = 0
+csv.close()
+
+
+def random_occupation(dict):
+    rand = random.random() * float(data[len(data)-1].split(',')[1])
+    sum = 0.0
     for occupation in dict:
-        if (occupation_count == len(dict)-1):
-            break
-        occupation_freq = int(dict[occupation] * 10)
-        for i in range(0, occupation_freq):
-            list.append(occupation)
-        occupation_count += 1
-    return random.choice(occupations_list)
+        sum += dict[occupation]
+        if rand < sum:
+            return occupation
 
 
-init_dict(occupations_dict, "occupations.csv")
-print(rand_occupation(occupations_dict, occupations_list))
+def testing():
+    frequency = {}
+    for occupation in occupations_dict:
+        frequency[occupation] = 0
+    for i in range(0, 100000):
+        rand = random_occupation(occupations_dict)
+        frequency[rand] += 1
+    return frequency
+
+
+init_dict()
+print('dictionary of occupations:\n' + str(occupations_dict) + '\n')
+print('random occupation: ' + random_occupation(occupations_dict) + '\n')
+print('frequencies for testing random occupation with 100000 trials:\n' + str(testing()))
