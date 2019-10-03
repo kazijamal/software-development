bf648eef6a48126df0a5ab9a1e9d25251588c229
Kazi Jamal <jamalkazi02@gmail.com>
2019-10-03 14:51:43 -0400

    added comments to 15_login

--- 15_login/app.py
+++ 15_login/app.py
@@ -8,30 +8,34 @@ import os
 
 app = Flask(__name__)
 
+# creates secret key for session
 app.secret_key = os.urandom(32)
+
+# hardcodes a single username/password combination
 testuser = "krispykreme"
 testpass = "12345678"
 
-
+# redirects to login page if no user logged in
+# redirects to welcome page if user logged in
 @app.route("/")
 def root():
     if "username" in session:
         return redirect(url_for('welcome'))
     return redirect(url_for('login'))
 
-
+# has logout button to log out
 @app.route("/welcome")
 def welcome():
     if "username" not in session:
         return redirect(url_for('root'))
     return render_template("welcome.html")
 
-
+# login page with form which sends request to /auth route
 @app.route("/login")
 def login():
     return render_template("login.html")
 
-
+# handles login request and redirects to welcome after adding username to session or returns an error
 @app.route("/auth", methods=["POST"])
 def auth():
     print("\n\n\n")
@@ -55,16 +59,15 @@ def auth():
         return error("Invalid Username")
         # return redirect(url_for('login'))
 
-
+# returns a page with provided error message
 def error(message):
     return render_template("error.html", error=message)
 
-
+# removes session data for username
 @app.route("/logout")
 def logout():
     session.pop('username')
     return redirect(url_for('root'))
-
 
 if __name__ == "__main__":
     app.debug = True
