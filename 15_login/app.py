6e4ca412eab4241c486ec2c151582f85cb879d88
Kazi Jamal <jamalkazi02@gmail.com>
2019-10-02 22:07:03 -0400

    added 15_login

--- 15_login/app.py
+++ 15_login/app.py
@@ -1,35 +1,68 @@
-from flask import Flask, request, render_template, session, redirect, url_for
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import session
+from flask import redirect
+from flask import url_for
+import os
 
 app = Flask(__name__)
 
+app.secret_key = os.urandom(32)
 testuser = "krispykreme"
 testpass = "12345678"
 
+
 @app.route("/")
 def root():
-    if request.args("username") == testuser:
-        if request.args("password") == testpass:
-            session["username"] = username
-    if session["username"] == testuser and session["password"] == testpass:
-        redirect(url_for(welcome))
-    else:
-        redirect(url_for(login))
+    if "username" in session:
+        return redirect(url_for('welcome'))
+    return redirect(url_for('login'))
+
 
 @app.route("/welcome")
 def welcome():
+    if "username" not in session:
+        return redirect(url_for('root'))
     return render_template("welcome.html")
+
 
 @app.route("/login")
 def login():
     return render_template("login.html")
 
+
+@app.route("/auth")
+def auth():
+    print("\n\n\n")
+    print("***DIAG: this Flask obj ***")
+    print(app)
+    print("***DIAG: request obj ***")
+    print(request)
+    print("***DIAG: request.args ***")
+    print(request.args)
+    if request.args['username'] == testuser:
+        if request.args['password'] == testpass:
+            session['username'] = request.args['username']
+            return redirect(url_for('welcome'))
+        else:
+            print("invalid password")
+            return redirect(url_for('login'))
+    else:
+        print("invalid username")
+        return redirect(url_for('login'))
+
+
 @app.route("/error")
 def error():
     return render_template("error.html")
 
+
 @app.route("/logout")
 def logout():
-    return render_template("logout.html")
+    session.pop('username')
+    return redirect(url_for('root'))
+
 
 if __name__ == "__main__":
     app.debug = True
