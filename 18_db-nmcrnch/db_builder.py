0eed584e7e1a51473c9f97ed2cfa728d5263bce1
Kazi Jamal <jamalkazi02@gmail.com>
2019-10-14 10:37:57 -0400

    updated 18_db-nmcrnch

---
+++ 18_db-nmcrnch/db_builder.py
@@ -0,0 +1,45 @@
+# Team Night Knight - Kazi Jamal and Nahi Khan
+# SoftDev1 pd9
+# K17 -- No Trouble
+# 2019-10-10
+
+import sqlite3  # enable control of an sqlite database
+import csv      # facilitate CSV I/O
+
+DB_FILE="school.db"
+
+db = sqlite3.connect(DB_FILE)   # open if file exists, otherwise create
+c = db.cursor()                 # facilitate db ops
+
+#=======================================================================
+
+# students table
+
+with open('students.csv') as studentscsv:
+    studentsReader = csv.DictReader(studentscsv)
+    # creates students table
+    createCommand = "CREATE TABLE students (name TEXT, age INTEGER, id INTEGER PRIMARY KEY);"
+    c.execute(createCommand)
+    # iterates through rows in csv as dictionaries
+    for row in studentsReader:
+        # inserts data from the current row into the students table
+        insertCommand = "INSERT INTO students VALUES (\"{}\", {}, {});".format(row['name'], row['age'], row['id'])
+        c.execute(insertCommand)
+
+# courses table
+
+with open('courses.csv') as coursescsv:
+    coursesReader = csv.DictReader(coursescsv)
+    # creates courses table
+    createCommand = "CREATE TABLE courses (code TEXT, mark INTEGER, id);"
+    c.execute(createCommand)
+    # iterates through rows in csv as dictionaries
+    for row in coursesReader:
+        # inserts data from the current row into the courses table
+        insertCommand = "INSERT INTO courses VALUES (\"{}\", {}, {})".format(row['code'], row['mark'], row['id'])
+        c.execute(insertCommand)
+
+#=======================================================================
+
+db.commit() # save changes
+db.close()  # close database
--- 18_db-nmcrnch/stu_mean.py
+++ 18_db-nmcrnch/stu_mean.py
@@ -1,4 +1,75 @@
 # Team Terminal Kangaroos - Tammy Chen and Kazi Jamal
 # SoftDev1 pd9
 # K18 -- Average
-# 2019-10-11
+# 2019-10-15
+
+import sqlite3 # enable control of an sqlite database
+import csv # faciliate CSV I/O
+
+DB_FILE = "school.db"
+
+db = sqlite3.connect(DB_FILE) # open if file exists, otherwise create
+c = db.cursor() # facilitate db ops
+
+gradesDict = {}
+avgDict = {}
+
+# initializes grades and average dictionaries with empty lists and 0.0 respectively
+def initDict():
+    q = "SELECT id FROM students;"
+    a = c.execute(q)
+    for line in a:
+        gradesDict[line[0]] = [];
+        avgDict[line[0]] = 0.0;
+
+# looks up each students grades and adds to grades dictionary
+def studentsGrades():
+    q = "SELECT name, students.id, mark FROM students, courses WHERE students.id = courses.id;"
+    a = c.execute(q)
+    # adds grades to grades dictionary with id as key and current grade is appended to the value
+    for line in a:
+        gradesDict[line[1]].append(line[2])
+
+# computes each students averages and adds to averages dictionary
+def studentsAverages():
+    for key in gradesDict:
+        count = 0;
+        # adds grades to average dictionary with id as key and grade gets added to the value
+        for grade in gradesDict[key]:
+            avgDict[key] += grade
+            count += 1
+        # the value for the current id in the average dictionary is divided by the number of grades to get the average
+        avgDict[key] /= count
+
+# prints each students name, id, and average
+def printAverages():
+    q = "SELECT name, id FROM students;"
+    a = c.execute(q)
+    # prints each student, their id, and their average
+    for line in a:
+        ans = "student: {}, id: {}, average: {}".format(line[0], line[1], avgDict[line[1]])
+        print(ans)
+
+# creates table of IDs and associated averages, named "stu_avg"
+def createAvgTable():
+    # creates stu_avg table in sqlite database
+    createCommand = "CREATE TABLE stu_avg (id INTEGER PRIMARY KEY, avg REAL);"
+    c.execute(createCommand)
+    # adds id and average as records in stu_avg table
+    for key in avgDict:
+        insertCommand = "INSERT INTO stu_avg VALUES ({}, {});".format(key, avgDict[key])
+        c.execute(insertCommand)
+
+# function to faciliate adding rows to the courses table
+def addCourses():
+    # INSERT CODE FOR ADDING COURSES FROM UPDATED CSV HERE
+
+initDict()
+studentsGrades()
+studentsAverages()
+printAverages()
+createAvgTable()
+addCourses()
+
+db.commit() # save changes
+db.close() # close database
