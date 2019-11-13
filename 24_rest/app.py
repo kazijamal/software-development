4d7bb15ae98664547ba49684c1492bf19508af09
Kazi Jamal <jamalkazi02@gmail.com>
2019-11-12 21:04:44 -0500

    finished 24_rest

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,4 +1,4 @@
-# Team koalas -- Tammy Chen and Kazi Jamal
+# Kazi Jamal
 # SoftDev1 pd9
 # K24 -- A RESTful Journey Skyward
 # 2019-11-12
@@ -10,10 +10,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    u = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=VLDCOQP6GcKTIqjBvRfwS8LvAAERQlx0n9f0Hwz5")
+    u = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=4P58G77ch5wxPDd5lZCCSZHJ7F4grda65M5qTphW")
     response = u.read()
     data = json.loads(response)
-    return render_template("index.html", pic = data['url'], explanation = data['explanation'])
+    return render_template("index.html", pic = data['url'], explanation = data['explanation'], title = data['title'])
 
 if __name__ == "__main__":
     app.debug = True
