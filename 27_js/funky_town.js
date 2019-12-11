56e689eea3de00f1e20338b7c9c3d261dee06553
Kazi Jamal <jamalkazi02@gmail.com>
2019-12-10 23:24:38 -0500

    completed 27_js

--- 27_js/funky_town.js
+++ 27_js/funky_town.js
@@ -1,13 +1,21 @@
 var foo = function() {
     console.log("foo testing");
-}
+};
+
+foo()
 
 var factorial = function(n) {
-    if (n == 1) {
+    if (n <= 1) {
 	return 1;
     }
     return n * factorial(n-1);
-}
+};
+
+console.log(factorial(1));
+console.log(factorial(2));
+console.log(factorial(3));
+console.log(factorial(4));
+console.log(factorial(5));
 
 var fibonacci = function(n) {
     if (n == 0) {
@@ -19,16 +27,40 @@ var fibonacci = function(n) {
     return fibonacci(n-1) + fibonacci(n-2);
 };
 
+console.log(fibonacci(0));
+console.log(fibonacci(1));
+console.log(fibonacci(2));
+console.log(fibonacci(3));
+console.log(fibonacci(4));
+console.log(fibonacci(5));
+
 var gcd = function(a, b) {
-    min = a < b ? a : b;
-    i = 0;
-    while (i < min) {
-	if (a % i == 0 && b % i == 0) {
-	    i++;
-	} else {
-	    i = i^2;
-	};
-    }
-    console.log(i);
-    return i;
-}
+    if (a == 0 || b == 0) {
+	return "a or b cannot be 0";
+    };
+    if (b > a) {
+	return gcd(b, a);
+    };
+    if (a % b == 0) {
+	return b;
+    };
+    return gcd(b, a % b);
+};
+
+console.log(gcd(18,3));
+console.log(gcd(3,18));
+console.log(gcd(0,0));
+console.log(gcd(48,18));
+	    
+var students = ["kazi", "albert", "nahi", "john", "jane"];
+
+var randomStudent = function(students) {
+    var randIndex = Math.floor(Math.random() * students.length);
+    return students[randIndex];
+};
+
+console.log(randomStudent(students));
+console.log(randomStudent(students));
+console.log(randomStudent(students));
+console.log(randomStudent(students));
+console.log(randomStudent(students));
