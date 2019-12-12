9a2850d7bc2ed1af954fa995d6c4d3eb1c41875a
Kazi Jamal <jamalkazi02@gmail.com>
2019-12-12 15:33:44 -0500

    29_jsdom working list

--- 29_jsdom/dommy.js
+++ 29_jsdom/dommy.js
@@ -4,10 +4,11 @@ var changeHeading = function(e) {
 };
 
 var removeItem = function(e) {
-    lis.splice(index, e);
+    e.remove();
 };
 
 var lis = document.getElementsByTagName("li");
+var count = lis.length
  
 for (var i=0; i < lis.length; i++) {
     lis[i].addEventListener('mouseover',
@@ -15,5 +16,48 @@ for (var i=0; i < lis.length; i++) {
     lis[i].addEventListener('mouseout',
 			    function(e) { changeHeading("Hello World!"); });
     lis[i].addEventListener('click',
-			    function(e) { removeItem(i); });
+			    function(e) { removeItem(e['target']); });
+};
+
+var addItem = function(e) {
+    console.log(e);
+    var list = document.getElementById("thelist");
+    var item = document.createElement("li");
+    item.innerHTML = "item " + count++;
+    item.addEventListener('mouseover',
+			    function(e) { changeHeading(e['target'].innerHTML); });
+    item.addEventListener('mouseout',
+			    function(e) { changeHeading("Hello World!"); });
+    item.addEventListener('click',
+			    function(e) { removeItem(e['target']); });
+    list.appendChild(item);
+};
+
+var button = document.getElementById("b");
+button.addEventListener('click', addItem);
+
+var fnum = 0;
+
+var fib = function(n) {
+    if (n < 2) {
+	return 1;
+    }
+    else {
+	return fib(n-1) + fib(n-2);
+    }
+};
+
+var addFib = function(e) {
+    console.log(e);
+    var list = document.getElementById("fiblist");
+    var item = document.createElement("li");
+    item.innerHTML = fib(fnum++);
+    list.appendChild(item);
 }
+
+var addFib2 = function(e) {
+    console.log(e);
+}
+
+var fb = document.getElementById("fb");
+fb.addEventListener('click', addFib);
