78a181891e6a8a26933d2c196e498b524f30d12f
Kazi Jamal <jamalkazi02@gmail.com>
2020-02-13 08:33:28 -0500

    updates

--- 07_canvas-anim/animation.js
+++ 07_canvas-anim/animation.js
@@ -11,52 +11,55 @@ const canvas = document.getElementById('playground');
 // instantiate a CanvasRenderingContext2D object
 const ctx = canvas.getContext('2d');
 
+// setting up global variables here to facilitate the animation
 var radius = 0;
 var change = 1;
 var animid;
 var isRunning = false;
 
-// clears the canvas
-var clearCanvas = function() {
-    console.log('clearing canvas');
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+// clearing canvas
+var clearCanvas = function () {
+     // console.log('clearing canvas');
+     ctx.clearRect(0, 0, canvas.width, canvas.height);
 };
 
-var start = function() {
-    console.log('start')
-    if (!isRunning) {
-        animate();
-        isRunning = true;
-    };
+// starting the animation
+var start = function () {
+     // console.log('start')
+     if (!isRunning) {
+          animate();
+          isRunning = true;
+     };
 }
 
-var animate = function() {
-    clearCanvas();
-    // Starts or resets current path
-    ctx.beginPath();
-    ctx.fillStyle = '#ff0000';
-    // Draws an arc.
-    ctx.arc(canvas.width/2, canvas.height/2, radius, 0, 2 * Math.PI);
-    ctx.fill();
-    // Fills the interior of the path,
-    // Will not work if the path has fewer than 3 points in it.
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = '#000000';
-    // Renders all the lines in the path, does not end the path.
-    ctx.stroke()
-    if (radius == 200) {
-	change = -1;
-    } else if (radius == 0) {
-	change = 1;
-    };
-    radius += change;
-    animid = window.requestAnimationFrame(animate);
+// the actual animation function
+var animate = function () {
+     clearCanvas();
+     // Starts or resets current path
+     ctx.beginPath();
+     ctx.fillStyle = '#00aaff';
+     // Draws an arc.
+     ctx.arc(canvas.width / 2, canvas.height / 2, radius * 1.5, 0, 2 * Math.PI);
+     ctx.fill();
+
+     // ctx.lineWidth = 2;
+     // ctx.strokeStyle = '#000000';
+     // ctx.stroke()
+     
+     if (radius == 200) {
+          change = -1;
+     } else if (radius == 0) {
+          change = 1;
+     };
+     radius += change;
+     animid = window.requestAnimationFrame(animate);
 };
 
-var stop = function() {
-    console.log('stop');
-    window.cancelAnimationFrame(animid);
-    isRunning = false;
+// stopping the animation
+var stop = function () {
+     // console.log('stop');
+     window.cancelAnimationFrame(animid);
+     isRunning = false;
 }
 
 // sets event listeners for animaniac
