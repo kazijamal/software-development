c9703b94d28a98a299f851a62725bc3cf2f786a2
Kazi Jamal <jamalkazi02@gmail.com>
2020-02-28 00:18:03 -0500

    finish 09_mongo

--- 09_mongo/yummymongopy.py
+++ 09_mongo/yummymongopy.py
@@ -12,19 +12,37 @@ items = []
 
 for item in dataset:
     items.append(loads(item))
-    
+
 db = client.primer
 
 restaurants = db.restaurants
 
 result = restaurants.insert_many(items)
 
-print('result =', result)
-
+# find restaurants by BOROUGH
 def findBorough(borough):
-    for restaurant in db.restaurants.find({"borough": borough}):
+    for restaurant in restaurants.find({'borough': borough}):
         pprint.pprint(restaurant)
-    
+
+# find restaurants by ZIPCODE
 def findZipcode(zipcode):
-    for restaurant in db.restaurants.find({"address.zipcode": zipcode}):
+    for restaurant in restaurants.find({'address.zipcode': zipcode}):
         pprint.pprint(restaurant)
+
+# find restaurants by ZIPCODE and =GRADE 
+def findZipcodeGrade(zipcode, grade):
+     for restaurant in restaurants.find({'address.zipcode': zipcode, 'grades': {'$elemMatch': {'grade': grade}}}):
+          pprint.pprint(restaurant)
+
+# find restaurants by ZIPCODE and <SCORE
+def findZipcodeScoreBelow(zipcode, score):
+     for restaurant in restaurants.find({'address.zipcode': zipcode, 'grades': {'$elemMatch': {'score': {'$lt': score}}}}):
+          pprint.pprint(restaurant)
+
+# ~creative~: find restaurants by BOROUGH, CUISINE and >=SCORE
+def findBoroughCuisineScore(borough, cuisine, score):
+     for restaurant in restaurants.find({'borough': borough, 'cuisine': cuisine, 'grades': {'$elemMatch': {'score': {'$gte': score}}}}):
+          pprint.pprint(restaurant)
+
+# clear
+restaurants.delete_many({})
