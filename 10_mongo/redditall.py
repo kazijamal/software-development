99e21e19cff4bd2964ad79bb579a40b29d62b7bd
Kazi Jamal <jamalkazi02@gmail.com>
2020-03-03 11:39:51 -0500

    Properly create database and add documents to collection

--- 10_mongo/redditall.py
+++ 10_mongo/redditall.py
@@ -1,7 +1,7 @@
 # Team rslashsoftdev -- Alice Ni and Kazi Jamal
 # Softdev pd9
 # K10 -- Import/Export Bank
-# 
+# 2020-03-04
 
 from pymongo import MongoClient
 import json
@@ -9,13 +9,16 @@ import pprint
 
 client = MongoClient('localhost', 27017)
 
-f = open('all.json', 'r')
-dataset = json.loads(f.read())
-f.close()
-
 db = client.reddit
+col = db['children']
+col.delete_many({})
 
-posts = db.posts
-
-# clear
-posts.delete_many({})
+if (col.count_documents({}) == 0):
+    f = open('all.json', 'r')
+    content = f.read()
+    f.close()
+    dataset = json.loads(content)
+    children = dataset['data']['children']
+    for child in children:
+        pprint.pprint(child)
+    col.insert_many(children)
