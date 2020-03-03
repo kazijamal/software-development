233a2c65bdc1c46048146cacbf887a94e1d5e9ec
Kazi Jamal <jamalkazi02@gmail.com>
2020-03-03 14:18:27 -0500

    Add import mechanism description

--- 10_mongo/redditall.py
+++ 10_mongo/redditall.py
@@ -7,7 +7,11 @@
 name of dataset: Reddit /r/all
 description of its contents: Contains posts from the Reddit subreddit /r/all
 hyperlink to where raw data is hosted: https://www.reddit.com/r/all.json
-brief summary of import mechanism: 
+brief summary of import mechanism:
+read the json file
+load the data as a json
+find the posts in a children array that is within the data object
+use insert_many to add the children array to the posts collection in our database 
 """
 
 from pymongo import MongoClient
@@ -20,14 +24,13 @@ db = client.rslashsoftdev
 posts = db['posts']
 posts.delete_many({})
 
-if (posts.count_documents({}) == 0):
-    f = open('all.json', 'r')
-    content = f.read()
-    f.close()
-    dataset = json.loads(content)
-    children = dataset['data']['children']
-    for child in children:
-        pprint.pprint(child)
+f = open('all.json', 'r')
+content = f.read()
+f.close()
+dataset = json.loads(content)
+children = dataset['data']['children']
+for child in children:
+    pprint.pprint(child)
     posts.insert_many(children)
 
 client.close()
