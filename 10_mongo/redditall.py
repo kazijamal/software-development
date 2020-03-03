70b8aae596910bb24bb90a5a41e595f88c9fac94
Kazi Jamal <jamalkazi02@gmail.com>
2020-03-03 11:45:22 -0500

    Update 10_mongo

--- 10_mongo/redditall.py
+++ 10_mongo/redditall.py
@@ -3,17 +3,24 @@
 # K10 -- Import/Export Bank
 # 2020-03-04
 
+"""
+name of dataset: Reddit /r/all
+description of its contents: Contains posts from the Reddit subreddit /r/all
+hyperlink to where raw data is hosted: https://www.reddit.com/r/all.json
+brief summary of import mechanism: 
+"""
+
 from pymongo import MongoClient
 import json
 import pprint
 
 client = MongoClient('localhost', 27017)
 
-db = client.reddit
-col = db['children']
-col.delete_many({})
+db = client.rslashsoftdev
+posts = db['posts']
+posts.delete_many({})
 
-if (col.count_documents({}) == 0):
+if (posts.count_documents({}) == 0):
     f = open('all.json', 'r')
     content = f.read()
     f.close()
@@ -21,4 +28,4 @@ if (col.count_documents({}) == 0):
     children = dataset['data']['children']
     for child in children:
         pprint.pprint(child)
-    col.insert_many(children)
+    posts.insert_many(children)
