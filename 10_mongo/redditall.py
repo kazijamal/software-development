5853421deffc9ba07d441d9142a4710b2745ce45
Kazi Jamal <jamalkazi02@gmail.com>
2020-03-03 14:47:54 -0500

    Add queries

--- 10_mongo/redditall.py
+++ 10_mongo/redditall.py
@@ -29,8 +29,37 @@ content = f.read()
 f.close()
 dataset = json.loads(content)
 children = dataset['data']['children']
-for child in children:
-    pprint.pprint(child)
-    posts.insert_many(children)
+posts.insert_many(children)
 
+def findSubreddit(subreddit):
+    print('----- found posts in r/' + subreddit + '\n')
+    for post in posts.find({'data.subreddit': subreddit}):
+        print(post['data']['title'] + '\n')
+
+def findGreaterScore(score):
+    print('----- found posts with score greater than ' + str(score) + '\n')
+    for post in posts.find({'data.score': {'$gt': score}}):
+        print(post['data']['title'] + ': ' + str(post['data']['score']) + '\n')
+
+def findLessCrossPosts(crossposts):
+    print('----- found posts with less than ' + str(crossposts) + ' crossposts\n')
+    for post in posts.find({'data.num_crossposts': {'$lt': crossposts}}):
+        print(post['data']['title'] + ': ' + str(post['data']['num_crossposts']) + '\n')
+
+def findGreaterEqualComments(comments):
+    print('----- found posts with greater than or equal to ' + str(comments) + ' comments\n')
+    for post in posts.find({'data.num_comments': {'$gte': comments}}):
+        print(post['data']['title'] + ': ' + str(post['data']['num_comments']) + '\n')
+
+def findGreaterScoreComments(score, comments):
+    print('----- found posts with greater than ' + str(score) + ' score and ' + str(comments) + ' comments\n')
+    for post in posts.find({'data.score': {'$gt': score}, 'data.num_comments': {'$gt': comments}}):
+        print(post['data']['title'] + ': ' + str(post['data']['score']) + ' score and ' + str(post['data']['num_comments']) + ' comments\n')
+        
+findSubreddit('Coronavirus')
+findGreaterScore(40000)
+findLessCrossPosts(1)
+findGreaterEqualComments(1046)
+findGreaterScoreComments(40000, 1046)
+            
 client.close()
