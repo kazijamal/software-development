4c7686267c6dc54674acd1653ee7fdd977bfb998
Kazi Jamal <jamalkazi02@gmail.com>
2020-04-21 23:01:14 -0400

    Finish 18_d3

--- 18_d3/static/js/script.js
+++ 18_d3/static/js/script.js
@@ -1,28 +1,31 @@
+import animateMap from "./utils/animate.js";
+import setupMap from "./utils/initMap.js";
+
 // App states
 let renderMap = false;
 let playing = false;
 
 // Handlers
 const toggleMapHandler = (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  if (!renderMap) {
-    renderMap = true;
-    setupMap();
-  }
+    if (!renderMap) {
+        renderMap = true;
+        setupMap();
+    }
 };
 
 const startHandler = (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  if (renderMap && !playing) {
-    playing = true;
-    animateMap();
-  }
+    if (renderMap && !playing) {
+        playing = true;
+        animateMap();
+    }
 };
 
-const renderBtn = document.getElementById('render-btn');
-renderBtn.addEventListener('click', toggleMapHandler);
+const renderBtn = document.getElementById("render-btn");
+renderBtn.addEventListener("click", toggleMapHandler);
 
-const startBtn = document.getElementById('start-btn');
-startBtn.addEventListener('click', startHandler);
+const startBtn = document.getElementById("start-btn");
+startBtn.addEventListener("click", startHandler);
--- 18_d3/static/js/utils/animate.js
+++ 18_d3/static/js/utils/animate.js
@@ -1,33 +1,41 @@
+import { getCOVIDData } from "./initData.js";
+
 // Control the animation of the state labels and colors
 const animateMap = () => {
-  const data = getCOVIDData();
+    const data = getCOVIDData();
 
-  // Map the domain difference between the beginning date of the dataset and the most recent date of the dataset to animation duration
-  const delay = d3
-    .scaleTime()
-    .domain([new Date(data[0].date), new Date(data[data.length - 1].date)])
-    .range([0, 20000]); // in milliseconds
+    // Map the domain difference between the beginning date of the dataset and the most recent date of the dataset to animation duration
+    const delay = d3
+        .scaleTime()
+        .domain([new Date(data[0].date), new Date(data[data.length - 1].date)])
+        .range([0, 10000]); // in milliseconds
 
-  // Map the domain (COVID-19 cases) to a range of rgb values
-  const stateColoring = d3
-    .scaleLinear()
-    .domain([0, 50000])
-    .range(['white', 'red']);
+    // Map the domain (COVID-19 cases) to a range of rgb values
+    const stateColoring = d3
+        .scaleLinear()
+        .domain([0, 50000])
+        .range(["white", "red"]);
 
-  // Go through each of the datapoints in us-states-covid.csv
-  for (const d of data) {
-    const date = document.getElementById('date');
-    const statePathElement = document.querySelector(
-      `path[data-state="${d.state}"]`
-    );
-    const stateTextElement = document.getElementById(d.state);
+    // Go through each of the datapoints in us-states-covid.csv
+    for (const d of data) {
+        const date = document.getElementById("date");
+        const statePathElement = document.querySelector(
+            `path[data-state="${d.state}"]`
+        );
+        const stateTextElement = document.getElementById(d.state);
 
-    if (!date || !statePathElement || !stateTextElement) continue; // ex: Virgin Islands is not on the map so either date, statePathElement or stateTextElement would be null
+        if (!date || !statePathElement || !stateTextElement) continue; // ex: Virgin Islands is not on the map so either date, statePathElement or stateTextElement would be null
 
-    d3.timeout(() => {
-      date.textContent = d.date;
-      statePathElement.style.fill = stateColoring(+d.cases);
-      stateTextElement.textContent = d.cases;
-    }, delay(new Date(d.date))); // Like before, the date string must be wrapped in a Date object!
-  }
+        d3.timeout(() => {
+            if (d.date == "2020-04-20") {
+              date.textContent = `${d.date} 🥦`
+            } else {
+            date.textContent = d.date;
+            }
+            statePathElement.style.fill = stateColoring(+d.cases);
+            stateTextElement.textContent = d.cases;
+        }, delay(new Date(d.date))); // Like before, the date string must be wrapped in a Date object!
+    }
 };
+
+export default animateMap;
